c6617f05a5c3b43e28221b8c6729162700966e80
Will Dady <willdady@gmail.com>
2023-05-16 17:11:31 +1000

    Initial commit

---
+++ test/aws-app-runner-scheduler.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAppRunnerScheduler from '../lib/aws-app-runner-scheduler-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-app-runner-scheduler-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAppRunnerScheduler.AwsAppRunnerSchedulerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
